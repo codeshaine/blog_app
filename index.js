2a66143a08cf707de4c5d66f093d7f25964830b5
shaine <shainilps05@gmail.com>
2023-12-07 14:27:12 +0530

    done with the inserting and deleting procedure

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-// app.use(express.static("script"));
 
 
 let storage = [];
@@ -20,7 +19,7 @@ app.get("/", (req, res) => {
   res.render("index", { storage });
 });
 
-app.post("/index", (req, res) => {
+app.post("/", (req, res) => {
   let newObj = obj(req.body);
   if (
     storage.length === 0 ||
@@ -30,32 +29,20 @@ app.post("/index", (req, res) => {
   } else {
     console.log("duplicate");
   }
-
-  // console.log(storage)
-  res.render("index", { storage });
+  res.redirect("/")
 });
 
-app.delete("/index/:index", (req, res) => {
 
-  console.log(req.params.index)
-  // i=req.params.index*1
-    let i = parseInt(req.params.index, 10);
-  // console.log(i);
-  if (i >= 0 && i < storage.length) {
-    storage.splice(i, 1);
-    // console.log(storage);
-      // res.render("partials/data", { storage });
-       setTimeout(() => {
-         res.render("partials/data", { storage });
-       }, 0);
-      // res.json({ storage });
-    // res.render("index", { storage });
-  } else {
-    res.sendStatus(404);
+app.post("/delete",(req,res)=>{
+  const index=req.body.index;
+  if(index>=0 && index<storage.length)
+  {
+   storage.splice(index,1)
+   console.log("deleted");
+   res.redirect("/")
   }
+})
 
-  //  res.status(204).send();
-  // res.redirect("/")
-});
+
 
 app.listen(3000, () => console.log("http://localhost:3000/"));
--- public/script/script.js
+++ public/script/script.js
@@ -1,21 +1,6 @@
-function deleteItem(index) {
-  console.log(index);
-  fetch(`/index/${index}`, {
-    method: "delete",
-  })
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error(`oops you got an error ${res.status}`);
-      }
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data);
-      //  updateUI(data.storage);
-      location.reload();
-      // renderUpdatedData(data.storage);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+function triggerSubmission(clicked) {
+  let form = clicked.closest("form");
+  console.log(clicked)
+  form.submit();
 }
+
