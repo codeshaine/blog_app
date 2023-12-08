550374853ca1e1965aa6b81a9e131d1c7cb564e8
shaine <shainilps05@gmail.com>
2023-12-08 21:17:59 +0530

    redoing commit after clearing files

--- public/script/script.js
+++ public/script/script.js
@@ -1,14 +1,10 @@
 function triggerSubmission(clicked) {
-
   const confirmation=window.confirm("Are you sure you want to delete this blog?")
   let form = clicked.closest("form");
 if(confirmation)
   form.submit();
 }
 
-function triggerUpdate(clicked) {
-  let form = clicked.closest("form");
-form.submit();
-}
+
 
 
