605f760893ada9e954c5cc92885f5fbc7e59eb9d
codeshaine <shainilps05@gmail.com>
2024-01-06 21:18:37 +0530

    new start to styles

--- public/script/script.js
+++ public/script/script.js
@@ -1,11 +1,8 @@
 function triggerSubmission(clicked) {
-  const confirmation=window.confirm("Are you sure you want to delete this blog?")
+  const confirmation = window.confirm(
+    "Are you sure you want to delete this blog?"
+  );
   let form = clicked.closest("form");
-  console.log("clieckkd")
-if(confirmation)
-  form.submit();
+  console.log("clieckkd");
+  if (confirmation) form.submit();
 }
-
-
-
-
